c1c337c8e7bfa30c988f636b56ac320dc96b092e
drussell0033@gmail.com <drussell0033@gmail.com>
2026-03-22 15:15:20 -0700

    Finised basic token for the userdto

--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b0da3a85a588b0543ac39dad13f404c522f6b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54f872687489c665c2bfa413e4764fa888c51582")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
