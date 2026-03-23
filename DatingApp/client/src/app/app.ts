01def3593c1122ec84a79f8770da4b5405bb4c05
drussell0033@gmail.com <drussell0033@gmail.com>
2026-03-22 21:45:51 -0700

    finished route guards and app init refactor

--- DatingApp/client/src/app/app.ts
+++ DatingApp/client/src/app/app.ts
@@ -1,10 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
-import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
-import { AccountService } from '../core/services/account-service';
-import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
@@ -12,32 +8,6 @@ import { User } from '../types/user';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit { 
-  private accountService = inject(AccountService);
-  protected router = inject(Router);
-  private http = inject(HttpClient);
-  protected title = signal('Dating App');
-  protected members = signal<User[]>([])
-
-  async ngOnInit() {
-      this.members.set(await this.getMembers());
-      this.setCurrentUser();
-  }
-
-  setCurrentUser() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user = JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async getMembers() {
-    try {
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-    } catch (error) {
-      console.log(error);
-      throw error;
-    }
-    
-  }
+export class App { 
+  protected router = inject(Router);    
 }
