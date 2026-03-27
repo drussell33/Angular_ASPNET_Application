e1780ce580176b4e152a6b455f9a45283ebff646
drussell0033@gmail.com <drussell0033@gmail.com>
2026-03-27 11:16:03 -0700

    Added profile edit form, submit buttons, and deactive guards for both in app and browser

--- DatingApp/client/src/features/members/member-profile/member-profile.ts
+++ DatingApp/client/src/features/members/member-profile/member-profile.ts
@@ -1,34 +1,46 @@
-import { Component, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
+import { Component, HostListener, inject, OnDestroy, OnInit, signal, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { EditableMember, Member } from '../../../types/member';
 import { DatePipe } from '@angular/common';
 import { MemberService } from '../../../core/services/member-service';
-import { NgForm } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { ToastService } from '../../../core/services/toast-service';
 
 @Component({
   selector: 'app-member-profile',
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css',
 })
 export class MemberProfile  implements OnInit, OnDestroy {
-  @ViewChild('editForm') editForm?: NgForm
+  @ViewChild('editForm') editForm?: NgForm;
+  @HostListener('window:beforeunload', ['$event']) notify($event:BeforeUnloadEvent) {
+    if (this.editForm?.dirty) {
+      $event.preventDefault();
+    }
+  } 
   protected memberService = inject(MemberService);
   private toast = inject(ToastService);
   private route = inject(ActivatedRoute);
   protected member = signal<Member | undefined>(undefined);
-  protected editableMember?: EditableMember;
+  protected editableMember: EditableMember = {
+    displayName: '',
+    description: '',
+    city: '',
+    country: ''
+  }
+
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe(data => {
       this.member.set(data['member']);
     })
+    
     this.editableMember = {
       displayName: this.member()?.displayName || '',
       description: this.member()?.description || '',
       city: this.member()?.city || '',
-      country: this.member()?.country || '',
+      country: this.member()?.country || ''
     }
   }
 
